7aa2049d8e75529dc91e149b85ee0927f49aea03
Juan Barcelos <juanbarcelos@outlook.com.br>
2022-11-06 00:05:23 -0300

    initial project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const Home = () => {
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setTopMovies(data.results);
+  };
+
+  useEffect(() => {
+    const topRateUrl = `${moviesURL}top_rated?${apiKey}`;
+    getTopRatedMovies(topRateUrl);
+  }, [])
+
+  return (
+    <div className="container">
+      <h2 className="title">Melhores Filmes</h2>
+      <div className="movies-container">
+        {topMovies.length > 0 && topMovies.map((movie) => <p>{movie.title}</p>)}
+      </div>
+    </div>
+  )
+};
+export default Home;
