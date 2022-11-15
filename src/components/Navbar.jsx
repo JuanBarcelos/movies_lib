89e1b2f7b97a57ccd5022044df2bea88caf9bff1
Juan Barcelos <juanbarcelos@outlook.com.br>
2022-11-14 23:24:06 -0300

    projeto finalizado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,23 @@
-import { Link } from 'react-router-dom'
-import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi'
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi';
+import "./NavBar.css";
 
 const Navbar = () => {
+
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!search) return;
+
+    navigate(`/search?q=${search}`);
+    setSearch('');
+  }
+
+
   return (
     <nav id="navbar">
       <h2>
@@ -9,8 +25,13 @@ const Navbar = () => {
           <BiCameraMovie /> MoviesLib
         </Link>
       </h2>
-      <form>
-        <input type='text' placeholder='Busque um filme' />
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Busque um filme'
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
         <button type='submit'>
           <BiSearchAlt2 />
         </button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import MoviesCard from "../components/MovieCard";
+import "./MovieGrid.css";
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
@@ -21,7 +23,8 @@ const Home = () => {
     <div className="container">
       <h2 className="title">Melhores Filmes</h2>
       <div className="movies-container">
-        {topMovies.length > 0 && topMovies.map((movie) => <p>{movie.title}</p>)}
+        {topMovies.length === 0 && <p>Carregando...</p>}
+        {topMovies.length > 0 && topMovies.map((movie) => <MoviesCard key={movie.id} movie={movie} />)}
       </div>
     </div>
   )
